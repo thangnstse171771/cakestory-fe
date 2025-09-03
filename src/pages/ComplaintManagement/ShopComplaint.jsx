4dd29ccb4214ba3209d05f4ed5f08a42018f5b3b
TruongNguyen07122003 <96875408+TruongNguyen07122003@users.noreply.github.com>
2025-09-03 19:36:49 +0700

    Refactor code complaints

--- src/pages/ComplaintManagement/ShopComplaint.jsx
+++ src/pages/ComplaintManagement/ShopComplaint.jsx
@@ -1,8 +1,13 @@
-import { useParams } from "react-router-dom";
-import ComplaintList from "./ComplaintList";
+// import { useParams } from "react-router-dom";
+// import ComplaintList from "./ComplaintList";
+
+// export default function ShopComplaint() {
+//   const { shopId } = useParams();
+//   // Delegate fetching and normalization to ComplaintList with provided shopId
+//   return <ComplaintList shopId={shopId} />;
+// }
+import React from "react";
 
 export default function ShopComplaint() {
-  const { shopId } = useParams();
-  // Delegate fetching and normalization to ComplaintList with provided shopId
-  return <ComplaintList shopId={shopId} />;
+  return <div>ShopComplaint</div>;
 }
